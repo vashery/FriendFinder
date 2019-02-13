e4ee5638b4346ab113f9f21aeeeb835f7fb660d3
lmccray <lmccray@uhin.org>
2019-02-12 19:58:23 -0700

    got person compatability calculator working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,19 @@ function difference(a, b) {
   return Math.abs(a - b);
 }
 
+function findSmallest(arr) {
+  var index = 0;
+  var value = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] < value) {
+      value = arr[i];
+      index = i;
+    }
+  }
+  return index;
+  console.log("value: " + value + " " + "index: " + index);
+}
+
 arrSum = function (arr) {
   return arr.reduce(function (a, b) {
     return a + b
@@ -22,24 +35,29 @@ module.exports = function (app) {
   app.post("/api/friends", function (req, res) {
 
 
-    console.log(req.body);
+    //console.log(req.body);
 
-
+    comparenumbers = [];
     for (let i = 0; i < friendData.length; i++) {
       currentcompare = [];
       var currentfriend = friendData[i].scores;
       for (let i = 0; i < currentfriend.length; i++) {
         currentcompare.push(difference(Number(currentfriend[i]), Number(req.body.scores[i])))
+
       }
-      comparenumbers.push(arrSum(currentcompare))
-    }
-    for (let i = 0; i < comparenumbers.length; i++) {
-      const element = comparenumbers[i];
+      comparenumbers.push(arrSum(currentcompare));
 
     }
+    findSmallest(comparenumbers)
+    console.log(comparenumbers)
+    //console.log(Math.min(comparenumbers));
+    //console.log(Math.min(comparenumbers));
+    //for (let i = 0; i < comparenumbers.length; i++) {
+    //  comparearray = comparenumbers[i];
+
+    //}
     friendData.push(req.body);
-    res.json(friendData);
-    console.log(friendData)
+    res.json(friendData[findSmallest(comparenumbers)]);
   });
 
   app.post("/api/clear", function (req, res) {
